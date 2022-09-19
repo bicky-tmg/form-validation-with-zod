c76522725d5d13412a9734bc0ae3d132b4f53b50
Bicky Tamang <bickytamang101@gmail.com>
2022-09-19 18:05:20 +0545

    feat: adding validation

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,6 +1,95 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const validationSchema = z
+  .object({
+    firstName: z.string().min(1, { message: "Firstname is required" }),
+    lastName: z.string().min(1, { message: "Lastname is required" }),
+    email: z.string().min(1, { message: "Email is required" }).email({
+      message: "Must be a valid email",
+    }),
+    password: z
+      .string()
+      .min(6, { message: "Password must be atleast 6 characters" }),
+    confirmPassword: z
+      .string()
+      .min(1, { message: "Confirm Password is required" }),
+    // terms: z.literal(true, {
+    //   invalid_type_error: "You must accept Terms and Conditions",
+    // }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Password don't match",
+  });
+
+const passwordForm = z
+  .object({
+    firstName: z.string().min(1, { message: "Firstname is required" }),
+    lastName: z.string().min(1, { message: "Lastname is required" }),
+    email: z.string().min(1, { message: "Email is required" }).email({
+      message: "Must be a valid email",
+    }),
+    password: z
+      .string()
+      .min(6, { message: "Password must be atleast 6 characters" }),
+    confirm: z.string().min(1, { message: "Confirm Password is required" }),
+    terms: z.literal(true),
+  })
+  .refine((data) => data.password === data.confirm, {
+    message: "Passwords don't match",
+    path: ["confirm"], // path of error
+  });
+
+try {
+  passwordForm.parse({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "asdf",
+    confirm: "qwer",
+    terms: "hi",
+  });
+} catch (err) {
+  if (err instanceof z.ZodError) {
+    console.log(err.issues);
+  }
+}
+
+// try {
+//   validationSchema.parse({
+//     firstName: "",
+//     lastName: "",
+//     email: "",
+//     password: "1234567",
+//     confirmPassword: "abcdefgh",
+//     terms: false,
+//   });
+// } catch (err) {
+//   if (err instanceof z.ZodError) {
+//     console.log(err.issues);
+//   }
+// }
+
+type ValidationSchema = z.infer<typeof validationSchema>;
+
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ValidationSchema>({
+    resolver: zodResolver(validationSchema),
+    criteriaMode: "all",
+  });
+
+  const onSubmit: SubmitHandler<ValidationSchema> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded">
+    <form className="px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-4 md:flex md:justify-between">
         <div className="mb-4 md:mr-2 md:mb-0">
           <label
@@ -10,11 +99,19 @@ const Form = () => {
             First Name
           </label>
           <input
-            className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"
+            className={`w-full px-3 py-2 text-sm leading-tight text-gray-700 border ${
+              errors.firstName && "border-red-500"
+            } rounded appearance-none focus:outline-none focus:shadow-outline`}
             id="firstName"
             type="text"
             placeholder="First Name"
+            {...register("firstName")}
           />
+          {errors.firstName && (
+            <p className="text-xs italic text-red-500 mt-2">
+              {errors.firstName?.message}
+            </p>
+          )}
         </div>
         <div className="md:ml-2">
           <label
@@ -24,11 +121,19 @@ const Form = () => {
             Last Name
           </label>
           <input
-            className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"
+            className={`w-full px-3 py-2 text-sm leading-tight text-gray-700 border ${
+              errors.lastName && "border-red-500"
+            } rounded appearance-none focus:outline-none focus:shadow-outline`}
             id="lastName"
             type="text"
             placeholder="Last Name"
+            {...register("lastName")}
           />
+          {errors.lastName && (
+            <p className="text-xs italic text-red-500 mt-2">
+              {errors.lastName?.message}
+            </p>
+          )}
         </div>
       </div>
       <div className="mb-4">
@@ -39,11 +144,19 @@ const Form = () => {
           Email
         </label>
         <input
-          className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"
+          className={`w-full px-3 py-2 text-sm leading-tight text-gray-700 border ${
+            errors.email && "border-red-500"
+          } rounded appearance-none focus:outline-none focus:shadow-outline`}
           id="email"
           type="email"
           placeholder="Email"
+          {...register("email")}
         />
+        {errors.email && (
+          <p className="text-xs italic text-red-500 mt-2">
+            {errors.email?.message}
+          </p>
+        )}
       </div>
       <div className="mb-4 md:flex md:justify-between">
         <div className="mb-4 md:mr-2 md:mb-0">
@@ -54,13 +167,18 @@ const Form = () => {
             Password
           </label>
           <input
-            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded appearance-none focus:outline-none focus:shadow-outline"
+            className={`w-full px-3 py-2 text-sm leading-tight text-gray-700 border ${
+              errors.password && "border-red-500"
+            } rounded appearance-none focus:outline-none focus:shadow-outline`}
             id="password"
             type="password"
+            {...register("password")}
           />
-          <p className="text-xs italic text-red-500">
-            Please choose a password.
-          </p>
+          {errors.password && (
+            <p className="text-xs italic text-red-500 mt-2">
+              {errors.password?.message}
+            </p>
+          )}
         </div>
         <div className="md:ml-2">
           <label
@@ -70,30 +188,40 @@ const Form = () => {
             Confirm Password
           </label>
           <input
-            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded appearance-none focus:outline-none focus:shadow-outline"
+            className={`w-full px-3 py-2 text-sm leading-tight text-gray-700 border ${
+              errors.confirmPassword && "border-red-500"
+            } rounded appearance-none focus:outline-none focus:shadow-outline`}
             id="c_password"
             type="password"
+            {...register("confirmPassword")}
           />
+          {errors.confirmPassword && (
+            <p className="text-xs italic text-red-500 mt-2">
+              {errors.confirmPassword?.message}
+            </p>
+          )}
         </div>
       </div>
-      <div className="mb-4">
-        <input
-          name="terms"
-          type="checkbox"
-          id="terms"
-          className={`form-check-input`}
-        />
+      {/* <div className="mb-4">
+        <input type="checkbox" id="terms" {...register("terms")} />
         <label
           htmlFor="terms"
-          className="ml-2 mb-2 text-sm font-bold text-gray-700"
+          className={`ml-2 mb-2 text-sm font-bold ${
+            errors.terms ? "text-red-500" : "text-gray-700"
+          }`}
         >
           Accept Terms & Conditions
         </label>
-      </div>
+        {errors.terms && (
+          <p className="text-xs italic text-red-500 mt-2">
+            {errors.terms?.message}
+          </p>
+        )}
+      </div> */}
       <div className="mb-6 text-center">
         <button
           className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-          type="button"
+          type="submit"
         >
           Register Account
         </button>
@@ -102,7 +230,7 @@ const Form = () => {
       <div className="text-center">
         <a
           className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
-          href="#"
+          href="#test"
         >
           Forgot Password?
         </a>
